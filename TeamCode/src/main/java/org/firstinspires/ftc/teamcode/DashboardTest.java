d0b9f129b63e34e80ddf343659bda246535174c6
jaredwen <pikaredwen@gmail.com>
2026-06-17 14:50:43 -0700

    this has heading and trans PIDF and pedo data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DashboardTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DashboardTest.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
 @TeleOp(name = "Dashboard Test ChargedCreeper", group = "Linear OpMode")
@@ -12,12 +14,18 @@ public class DashboardTest extends OpMode {
     public static double FLYWHEEL_VELOCITY = 2600;
 
     private DcMotorEx flywheel;
+    private CRServo leftServo;
+    private CRServo rightServo;
+    private boolean servoIsRunning;
 
     @Override
     public void init() {
         flywheel = hardwareMap.get(DcMotorEx.class, "flywheel");
         flywheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftServo = hardwareMap.get(CRServo.class, "leftServo");
+        rightServo = hardwareMap.get(CRServo.class, "rightServo");
+        servoIsRunning = false;
     }
 
     @Override
@@ -26,5 +34,19 @@ public class DashboardTest extends OpMode {
         telemetry.addData("Target Velocity", FLYWHEEL_VELOCITY);
         telemetry.addData("Actual Velocity", flywheel.getVelocity());
         telemetry.update();
+        if (gamepad1.a) {
+            servoIsRunning = true;
+        } else if (gamepad1.b) {
+            servoIsRunning = false;
+        }
+        if (servoIsRunning) {
+            leftServo.setPower(-1);
+            rightServo.setPower(1);
+
+        } else {
+            leftServo.setPower(0);
+            rightServo.setPower(0);
+            flywheel.setVelocity(0);
+        }
     }
 }
