f9928daffa58587756bc774a68752678daefbcbd
zhihua wen <zhihua.wen@snapchat.com>
2026-06-20 11:09:46 -0700

    add apirl tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
@@ -39,14 +39,19 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 /*
@@ -72,6 +77,7 @@ public class BasicOpMode_Linear extends OpMode {
     private GoBildaPinpointDriver odo;
     private double targetHeading = 0;
     private Pose2D previousPos;
+    private boolean exposureConfigured = false;
 
     private static final boolean USE_WEBCAM = true; // true for webcam, false for phone camera
     private AprilTagProcessor aprilTag;
@@ -114,71 +120,39 @@ public class BasicOpMode_Linear extends OpMode {
     }
 
     private void initAprilTag() {
-
-        // Create the AprilTag processor.
-        aprilTag =
-                new AprilTagProcessor.Builder()
-
-                        // The following default settings are available to un-comment and edit as
-                        // needed.
-                        // .setDrawAxes(false)
-                        // .setDrawCubeProjection(false)
-                        // .setDrawTagOutline(true)
-                        // .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-                        // .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
-                        // .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
-
-                        // == CAMERA CALIBRATION ==
-                        // If you do not manually specify calibration parameters, the SDK will
-                        // attempt
-                        // to load a predefined calibration for your camera.
-                        // .setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
-                        // ... these parameters are fx, fy, cx, cy.
-
+        AprilTagLibrary testLibrary =
+                new AprilTagLibrary.Builder()
+                        .addTag(6, "FIH", 6.8125, DistanceUnit.INCH)
+                        .addTag(24, "RED", 6.5, DistanceUnit.INCH)
+                        .addTag(20, "BLUE", 6.5, DistanceUnit.INCH)
                         .build();
 
-        // Adjust Image Decimation to trade-off detection-range for detection-rate.
-        // eg: Some typical detection data using a Logitech C920 WebCam
-        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
-        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
-        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
-        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
-        // Note: Decimation can be changed on-the-fly to adapt during a match.
-        // aprilTag.setDecimation(3);
+        aprilTag =
+                new AprilTagProcessor.Builder()
+                        .setDrawTagOutline(true)
+                        .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                        .setTagLibrary(testLibrary)
+                        .setLensIntrinsics(243.0, 243.0, 320.0, 240.0)
+                        .build();
 
-        // Create the vision portal by using a builder.
+        aprilTag.setDecimation(3);
+
         VisionPortal.Builder builder = new VisionPortal.Builder();
-
-        // Set the camera (webcam vs. built-in RC phone camera).
         if (USE_WEBCAM) {
             builder.setCamera(hardwareMap.get(WebcamName.class, "NeverGonnaGiveYouUp"));
         } else {
             builder.setCamera(BuiltinCameraDirection.BACK);
         }
 
-        // Choose a camera resolution. Not all cameras support all resolutions.
         builder.setCameraResolution(new Size(640, 480));
-
-        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
         builder.enableLiveView(true);
-
-        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
         builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
-
-        // Choose whether or not LiveView stops if no processors are enabled.
-        // If set "true", monitor shows solid orange screen if no processors enabled.
-        // If set "false", monitor shows camera view without annotations.
         builder.setAutoStopLiveView(false);
-
-        // Set and enable the processor.
         builder.addProcessor(aprilTag);
 
-        // Build the Vision Portal, using the above settings.
         visionPortal = builder.build();
-
-        // Disable or re-enable the aprilTag processor at any time.
         visionPortal.setProcessorEnabled(aprilTag, true);
-    } // end method initAprilTag()
+    }
 
     @Override
     public void loop() {
@@ -344,4 +318,31 @@ public class BasicOpMode_Linear extends OpMode {
 
         previousPos = pos;
     }
+
+    private void setManualExposure(int exposureMS, int gain) {
+        if (visionPortal == null) return;
+
+        ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+        if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+            exposureControl.setMode(ExposureControl.Mode.Manual);
+        }
+        exposureControl.setExposure((long) exposureMS, TimeUnit.MILLISECONDS);
+
+        GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+        gainControl.setGain(gain);
+    }
+
+    @Override
+    public void init_loop() {
+        if (!exposureConfigured
+                && visionPortal.getCameraState() == VisionPortal.CameraState.STREAMING) {
+
+            setManualExposure(6, 50);
+            exposureConfigured = true;
+        }
+
+        telemetry.addLine("Ready to start");
+        telemetry.update();
+    }
+
 }
